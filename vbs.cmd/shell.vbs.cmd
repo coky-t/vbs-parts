2db51ab381d1da4443e1a281fdddb6d89c069645
Koki Takeyama <koki.takeyama@gmail.com>
2022-04-06 21:48:31 +0900

    initial commit - shell.vbs.cmd

---
+++ vbs.cmd/shell.vbs.cmd
@@ -0,0 +1,16 @@
+REM: %~nx0 & @cscript.exe //e:vbscript //nologo "%~f0" %* & GOTO :EOF
+
+Do While True
+    WScript.StdOut.Write("vbs>")
+    Line = WScript.StdIn.ReadLine
+    If LCase(Trim(Line)) = "exit" Then Exit Do
+    Exec Line
+Loop
+
+Sub Exec(Line)
+    On Error Resume Next
+    Err.Clear
+    Execute Line
+    If Err.Number <> 0 Then WScript.StdOut.WriteLine(Err.Description)
+    On Error Goto 0
+End Sub
