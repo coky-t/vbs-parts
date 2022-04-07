b0bb5cc02678627fba272841bc09cac77211908d
Koki Takeyama <koki.takeyama@gmail.com>
2022-04-07 23:13:16 +0900

    add one-liner functionality

--- vbs.cmd/shell.vbs.cmd
+++ vbs.cmd/shell.vbs.cmd
@@ -1,7 +1,12 @@
 REM: %~nx0 & @cscript.exe //e:vbscript //nologo "%~f0" %* & GOTO :EOF
 
+If WScript.Arguments.Length = 1 Then
+    Exec Replace(WScript.Arguments(0),"`","""")
+    WScript.Quit
+End If
+
 Do While True
-    WScript.StdOut.Write("vbs>")
+    WScript.StdOut.Write "vbs>"
     Line = WScript.StdIn.ReadLine
     If LCase(Trim(Line)) = "exit" Then Exit Do
     Exec Line
